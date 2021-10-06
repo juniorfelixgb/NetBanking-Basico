dbff834a3b6807715ed0845b3a88283670e90ecc
darl22 <corderodarlyn@gmail.com>
2021-10-06 08:45:13 -0400

    actualizacion

---
+++ src/NetBanking/NetBanking.DATA/Modelo/EstadoTrd.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace NetBanking.DATA.Modelo
+{
+    public partial class EstadoTrd
+    {
+        public EstadoTrd()
+        {
+            Depositos = new HashSet<Deposito>();
+            Retiros = new HashSet<Retiro>();
+            Transacciones = new HashSet<Transaccione>();
+        }
+
+        public int EstadoId { get; set; }
+        public string Estado { get; set; }
+
+        public virtual ICollection<Deposito> Depositos { get; set; }
+        public virtual ICollection<Retiro> Retiros { get; set; }
+        public virtual ICollection<Transaccione> Transacciones { get; set; }
+    }
+}
---
+++ src/NetBanking/NetBanking.DATA/Modelo/Moneda.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace NetBanking.DATA.Modelo
+{
+    public partial class Moneda
+    {
+        public Moneda()
+        {
+            Cuenta = new HashSet<Cuenta>();
+            MonedaCambioMonedaIddesdeNavigations = new HashSet<MonedaCambio>();
+            MonedaCambioMonedaIdhaciaNavigations = new HashSet<MonedaCambio>();
+        }
+
+        public int MonedaId { get; set; }
+        public string MonedaNombre { get; set; }
+        public string Simbolo { get; set; }
+
+        public virtual ICollection<Cuenta> Cuenta { get; set; }
+        public virtual ICollection<MonedaCambio> MonedaCambioMonedaIddesdeNavigations { get; set; }
+        public virtual ICollection<MonedaCambio> MonedaCambioMonedaIdhaciaNavigations { get; set; }
+    }
+}
