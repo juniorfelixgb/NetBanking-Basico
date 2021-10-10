5baca07ab66214d55245de129583a7d5b2c29fdc
alfredo <alfredocapellan20@gmail.com>
2021-10-10 09:58:43 -0400

    add modal and functionality to btn continuar

--- src/NetBanking/NetBanking.UI/wwwroot/js/site.js
+++ src/NetBanking/NetBanking.UI/wwwroot/js/site.js
@@ -2,6 +2,17 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-(function () {
-    
-})();
+
+
+//button de ventana transferenciaDestino
+
+inputMonto = document.getElementById("FormMonto");
+btnContinuar = document.getElementById("btnContinuar");
+
+inputMonto.addEventListener("input", () => {
+    if (inputMonto.value > 0) {
+        btnContinuar.disabled = false;
+    } else {
+        btnContinuar.disabled = true;
+    }
+});
